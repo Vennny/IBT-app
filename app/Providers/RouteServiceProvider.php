1e8c90116ecd8becf3292f9d3da2305225f5f207
Václav Trampeška <trampeskav@gmail.com>
2021-02-09 02:24:05 +0100

    Changed app name and deleted home view & routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const MAIN = '/';
 
     /**
      * The controller namespace for the application.
