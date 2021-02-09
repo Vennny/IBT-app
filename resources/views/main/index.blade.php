44974ffadb5b3298cf795d4f2612c1951da2b2e1
Václav Trampeška <trampeskav@gmail.com>
2021-02-09 01:30:57 +0100

    Unified default layout

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,14 +1,20 @@
-@extends('layouts.base')
+@extends('layouts.app')
 
 @section('title', 'Query Builder')
 
 @section('content')
+    <div id="loader">
+        <div class="loader_text" style="display: none;">
+            <h1>This might take a few minutes...</h1>
+        </div>
+        <div class="icon" style="display: none;"></div>
+    </div>
     <div class="administration-form">
         <br>
         <h1>Bulid Query</h1>
         <br>
 
-        <form action="/" method="POST">
+        <form id="query_builder" action="/" method="POST">
             @csrf
 
             <div class="form-group">
@@ -26,6 +32,15 @@
                     </select><br>
                 </div>
 
+                <div class="language">
+                    <label for="language">In language:</label>
+                    <select id="language" name="language">
+                        @foreach($languages as $lang)
+                            <option value="{{$lang->id}}">{{$lang->show_name}}</option>
+                        @endforeach
+                    </select><br>
+                </div>
+
                 <div style="display: none" class="countries_datalist">
                     <label for="country">From player from country: </label>
                     <input list="country" name="country" class="datalist-input" />
@@ -37,40 +52,36 @@
                     </datalist>
                 </div>
 
-                <div class="language">
-                    <label for="language">In language:</label>
-                    <select id="language" name="language">
-                        @foreach($languages as $lang)
-                            <option value="{{$lang->id}}">{{$lang->show_name}}</option>
-                        @endforeach
-                    </select><br>
-                </div>
-
-                <div class="limit">
-                    <label for="limit">Select number of entries</label>
-                    <input type="number" id="limit" name="limit" min="1" value="5"><br>
-                </div>
-
-
                 <div style="display: none" class="category">
-                    <label for="category">Category name</label><br>
+                    <label for="category">Category name:</label>
                     <input type="text" id="category" name="category" value=""><br>
+                </div>
+                <div style="display: none" class="letter">
+                    <label for="letter">Starting letter:</label>
+                    <input type="text" id="letter" name="letter" maxlength="1" value=""><br>
                 </div>
 
                 <div hidden class="word">
                     <label for="word">Word</label><br>
                     <input type="text" id="word" name="word" value=""><br>
                 </div>
+
+                <div class="limit">
+                    <label for="limit">Select number of entries</label>
+                    <input type="number" id="limit" name="limit" min="1" value="5"><br>
+                </div>
             </div>
 
 
-            <button type="submit" class="btn btn-primary">Confirm</button>
+            <button onclick="sendQuery()" class="btn btn-primary" >Confirm</button>
         </form>
     </div>
 @endsection
 @push('scripts')
-    <script src="{{url( 'vendor/jquery.min.js' )}}"></script>
     <script>
+
+
+
         $(document).ready(function(){
             /*
             let countries = {!! json_encode($countries, JSON_HEX_TAG) !!};
@@ -104,10 +115,13 @@
                 if(this.value === 'word'){
                   document.getElementsByClassName('countries_datalist')[0].style.display = 'block';
                   document.getElementsByClassName('category')[0].style.display = 'block';
+                    document.getElementsByClassName('letter')[0].style.display = 'block';
                 }
                 else {
                   document.getElementsByClassName('countries_datalist')[0].style.display = 'none';
-                  document.getElementsByClassName('category')[0].style.display = 'none';}
+                  document.getElementsByClassName('category')[0].style.display = 'none';
+                    document.getElementsByClassName('letter')[0].style.display = 'none';
+                }
             });
 
             $('#country').change(function() {
